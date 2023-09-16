d6da0b19eeae51c94163d602f56e47bff3760e10
parthiv8848 <124236867+parthiv8848@users.noreply.github.com>
2023-09-16 14:10:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Badge from '@mui/material/Badge'; // If you're using Material-UI v5 (beta)
+import Badge from '@mui/material/Badge';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import AddIcon from '@mui/icons-material/Add';
