9882b858e51df4986dae139f53bd04963dd4d0ba
parthiv8848 <124236867+parthiv8848@users.noreply.github.com>
2023-09-16 11:05:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'  //npm i bootstrap-dark-5 boostrap
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import Badge from '@mui/material/Badge'; // If you're using Material-UI v5 (beta)
+
 
 import Home from './screens/Home';
 import {
